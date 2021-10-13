4497e5ab44ecbd22bcca76950260c53e644b9f10
Tajwaar Shafiq <tajwaar.shafiq@mail.utoronto.ca>
2021-10-13 10:32:03 -0400

    Finished classes Group, Student, StudentManager, LogIn for phase0

---
+++ phase0/SkeletonProgram/Group.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Group {
+
+    private ArrayList<Student> students;
+    private String name;
+    private String gID;
+
+    public Group(String name) {
+        this.students = new ArrayList<Student>();
+        this.name = name;
+        this.gID = null;
+    }
+
+    public ArrayList<Student> getStudentsInGroup() {
+        return students;
+    }
+
+    public void setStudentsInGroup(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public String getGroupName() {
+        return name;
+    }
+
+    public void setGroupName(String name) {
+        this.name = name;
+    }
+
+    public String getgID() {
+        return gID;
+    }
+
+    public void setgID(String gID) {
+        this.gID = gID;
+    }
+}
