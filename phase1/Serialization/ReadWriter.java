a78aa87229c899ed483d8c56d18649a71a326021
Cathlyn <cathlynchen18@gmail.com>
2021-11-01 11:54:52 -0400

    Created ReadWriter interface

---
+++ phase1/Serialization/ReadWriter.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public interface ReadWriter {
+    void saveToFile(String filepath, Object o) throws IOException;
+
+    userMap throws IOException, ClassNotFoundException;
+}
