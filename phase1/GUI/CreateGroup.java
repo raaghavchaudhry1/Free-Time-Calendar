3c911f712d0c195cc979f73aecd06945121ec60c
raaghavchaudhrywork <raaghav.chaudhry@mail.utoronto.ca>
2021-11-05 16:44:05 -0400

    Raaghav's Commit

---
+++ phase1/GUI/CreateGroup.java
@@ -0,0 +1,2 @@
+public class CreateGroup {
+}
--- phase1/GUI/GroupMenu.java
+++ phase1/GUI/GroupMenu.java
@@ -16,40 +16,48 @@ public class GroupMenu implements ActionListener {
         this.frame = new JFrame();
         this.frame.setLayout(null);
         this.frame.setSize(500,500);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        this.createGroup = new JButton();
+        this.createGroup.setBounds(10,80,80,25);
+        this.createGroup.addActionListener(this);
+        this.frame.add(this.createGroup);
+        this.joinGroup = new JButton();
+        this.joinGroup.setBounds(110,80,80,25);
+        this.joinGroup.addActionListener(this);
+        this.frame.add(this.createGroup);
+        this.viewGroups = new JButton();
+        this.viewGroups.setBounds(110,80,80,25);
+        this.viewGroups.addActionListener(this);
+        this.frame.add(this.createGroup);
+        this.frame.setVisible(true);
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == this.viewGroups) {
+
+            this.frame.dispose();
+            ViewGroups viewGroups = new ViewGroups();
+
+
+        } else if (e.getSource() == this.createGroup) {
+
+            this.frame.dispose();
+            CreateGroup createGroup = new CreateGroup();
+
+
+
+        } else {
+
+            this.frame.dispose();
+            JoinGroup joinGroup = new JoinGroup();
+
+
+
+
+        }
+
 
     }
 }
