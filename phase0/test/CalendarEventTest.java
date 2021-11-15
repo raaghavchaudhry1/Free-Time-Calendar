454e47096a70e78b55ad8a1212fc101d00efa358
Cathlyn <cathlynchen18@gmail.com>
2021-11-15 08:44:19 -0500

    Updated CalendarEventTest

--- phase0/test/CalendarEventTest.java
+++ phase0/test/CalendarEventTest.java
@@ -16,14 +16,29 @@ public class CalendarEventTest {
     }
 
     @Test(timeout = 50)
-    public void TestGetDuration() {
-        assertEquals(1.0f, event.getDuration(), 0);
-    }
-
-    @Test(timeout = 50)
     public void TestCompareTo() {
         CalendarEvent tut = new CalendarEvent("Tutorial", 2.0f, 5.0f, "Wednesday");
         assertEquals(1, event.compareTo(tut));
+    }
+
+    @Test(timeout = 50)
+    public void TestStartTime() {
+        event.setStartTime(1.0f);
+        assertEquals(1.0f, event.getStartTime(), 0);
+        assertEquals(3.0f, event.getDuration(), 0);
+    }
+
+    @Test(timeout = 50)
+    public void TestEndTime() {
+        event.setEndTime(6.0f);
+        assertEquals(6.0f, event.getEndTime(), 0);
+        assertEquals(3.0f, event.getDuration(), 0);
+    }
+
+    @Test(timeout = 50)
+    public void TestName() {
+        event.setName("Sleep");
+        assertEquals("Sleep", event.getName());
     }
 
 }
