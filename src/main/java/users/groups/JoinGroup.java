4311b84558b41ce3a0489f8a0151a0db64bdbc8b
Gilbert Ye <90408501+GilbertYeUofT@users.noreply.github.com>
2021-12-04 17:56:06 -0500

    Revert "Revert "Refactored GUI interfaces""

--- src/main/java/users/groups/JoinGroup.java
+++ src/main/java/users/groups/JoinGroup.java
@@ -1,6 +1,7 @@
 package users.groups;
 
 import calendar.CalendarController;
+import frontend.GUI;
 import frontend.MainMenu;
 import login.LogIn;
 import users.students.Student;
@@ -12,7 +13,7 @@ import java.awt.event.ActionListener;
 
 /** CreateGroup window is made to join an existing group.*/
 
-public class JoinGroup implements ActionListener {
+public class JoinGroup implements GUI {
 
     private JFrame frame;
     private JTextField groupName;
@@ -36,15 +37,22 @@ public class JoinGroup implements ActionListener {
 
     public JoinGroup(LogIn loginController, GroupController groupController, CalendarController calendarController,
                      StudentController studentController, String studentUsername){
-        this.loginController = loginController;
-        this.groupController = groupController;
-        this.calendarController = calendarController;
-        this.studentController = studentController;
+        setControllers(loginController, groupController, calendarController, studentController);
         this.studentUsername = studentUsername;
-
         this.frame = new JFrame();
         this.frame.setLayout(null);
         this.frame.setSize(500,500);
+        setButtons();
+        setLabelsAndText();
+        this.frame.setVisible(true);
+    }
+
+    @Override
+    public void setLabelsAndText() {
+        JLabel error = new JLabel();
+        error.setText("Invalid Group ID! Try Again!");
+        error.setBounds(100, 100, 300, 300);
+        this.error = error;
 
         JLabel groupNameLabel = new JLabel("Group ID: ");
         groupNameLabel.setBounds(110,50,115, 30);
@@ -53,7 +61,10 @@ public class JoinGroup implements ActionListener {
         this.groupName = new JTextField();
         this.groupName.setBounds(210,50,165,30);
         this.frame.add(this.groupName);
+    }
 
+    @Override
+    public void setButtons() {
         this.submitButton = new JButton("Submit");
         this.submitButton.setBounds(100,100,100,30);
         this.submitButton.addActionListener(this);
@@ -63,16 +74,14 @@ public class JoinGroup implements ActionListener {
         this.cancelButton.setBounds(240,100,100,30);
         this.cancelButton.addActionListener(this);
         this.frame.add(this.cancelButton);
+    }
 
-        JLabel error = new JLabel();
-        error.setText("Invalid Group ID! Try Again!");
-        error.setBounds(100, 100, 300, 300);
-        this.error = error;
-
-
-
-
-        this.frame.setVisible(true);
+    @Override
+    public void setControllers(LogIn loginController, GroupController groupController, CalendarController calendarController, StudentController studentController) {
+        this.loginController = loginController;
+        this.groupController = groupController;
+        this.calendarController = calendarController;
+        this.studentController = studentController;
     }
 
     /** Makes button to perform based on a choice of a user.
--- src/main/java/users/groups/ViewGroups.java
+++ src/main/java/users/groups/ViewGroups.java
@@ -1,6 +1,7 @@
 package users.groups;
 
 import calendar.CalendarController;
+import frontend.GUI;
 import frontend.MainMenu;
 import login.LogIn;
 import users.students.Student;
@@ -15,7 +16,7 @@ import java.util.HashMap;
 
 /** ViewGroups window is to view current joined groups.*/
 
-public class ViewGroups implements ActionListener {
+public class ViewGroups implements GUI {
 
     private LogIn loginController;
     private GroupController groupController;
@@ -24,7 +25,7 @@ public class ViewGroups implements ActionListener {
     private String studentUsername;
     private JFrame frame;
     private HashMap<JButton, String> mappings;
-    private  JButton back;
+    private JButton back;
     private JLabel noGroups;
 
     /** constructor ViewGroups with 5 parameters
@@ -39,28 +40,32 @@ public class ViewGroups implements ActionListener {
                       StudentController studentController, String studentUsername)  {
 
         this.mappings = new HashMap<JButton, String>();
+        this.frame = new JFrame();
+        frame.setSize(750, 750);
+        setControllers(loginController, groupController, calendarController, studentController);
+        this.studentUsername = studentUsername;
+        this.frame.setLayout(new GridLayout(6, 6));
+        setButtons();
+        setLabelsAndText();
+        this.frame.setVisible(true);
+    }
+
+    @Override
+    public void setLabelsAndText() {
+
+    }
+
+    @Override
+    public void setButtons() {
         this.back = new JButton("Return Home");
         this.back.setBounds(0,0,200,200);
         this.back.addActionListener(this);
+        this.frame.add(back);
 
-        this.frame = new JFrame();
-        frame.setSize(750, 750);
-
-        this.loginController = loginController;
-        this.groupController = groupController;
-        this.calendarController = calendarController;
-        this.studentController = studentController;
-        this.studentUsername = studentUsername;
-        this.frame.setLayout(new GridLayout(6, 6));
         Student curr = studentController.getAllStudents().get(this.studentUsername);
         ArrayList<ArrayList<Object>> groups = this.groupController.getStudentGroups(curr);
         this.noGroups = new JLabel("You aren't in any Groups!");
         this.noGroups.setBounds(600, 600, 200, 200 );
-        this.frame.add(back);
-
-        if (groups.isEmpty()) {
-            this.frame.add(this.noGroups);
-        }
         for (ArrayList<Object> temp : groups) {
             String s = (String) temp.get(1);
             String s1 = String.format("View Group: %s", s);
@@ -70,7 +75,17 @@ public class ViewGroups implements ActionListener {
             this.frame.add(button);
             this.mappings.put(button, groupID);
         }
-        this.frame.setVisible(true);
+        if (groups.isEmpty()) {
+            this.frame.add(this.noGroups);
+        }
+    }
+
+    @Override
+    public void setControllers(LogIn loginController, GroupController groupController, CalendarController calendarController, StudentController studentController) {
+        this.loginController = loginController;
+        this.groupController = groupController;
+        this.calendarController = calendarController;
+        this.studentController = studentController;
     }
 
     /** Makes button to perform based on a choice of a user.
