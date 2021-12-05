7dc41eb32afcabd7a5ea106f670abc98c716d82a
Tajwaar Shafiq <tajwaar.shafiq@mail.utoronto.ca>
2021-12-05 10:38:31 -0500

    Refactored code to implement Factory Design Pattern (refactored EventCreator.java -> EventFactory.java)

--- src/main/java/events/EventFactory.java
+++ src/main/java/events/EventFactory.java
@@ -1,6 +1,6 @@
 package events;
 
-public class EventCreator {
+public class EventFactory {
     public CalendarEvent createEvent(String name, float start, float end, String day) {
         // if recurring and not given a date
         return new CalendarEvent(name, start, end, day);
