08d4ebbe2063b73f0170bd08c20150d6c0f0ec4b
Cathlyn <cathlynchen18@gmail.com>
2021-12-05 21:41:57 -0500

    Deleted serialization attempt

--- phase1/Serialization/ReadWriter.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-
-public interface ReadWriter {
-    void saveToFile(String filepath, Object o) throws IOException;
-
-    userMap throws IOException, ClassNotFoundException;
-}
